e08da9e5558245cd2a4459b96b8f12fb6e2cc886
Caleb Duncan <cduncan4@my.westga.edu>
2010-10-07 12:21:25 -0400

    "Added sales Form"

---
+++ MemberInfo/SaleForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MemberInfo
+{
+    public partial class SaleForm : Form
+    {
+        public SaleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createNewMemButton_Click(object sender, EventArgs e)
+        {
+            NewMemberForm theMemberForm = new NewMemberForm();
+            theMemberForm.Show();
+        }
+    }
+}
